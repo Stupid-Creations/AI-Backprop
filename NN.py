51aaa29d668a8d0238e778dedf23e1ce986a6e60
Stupid-Creations <97241429+Stupid-Creations@users.noreply.github.com>
2023-05-12 13:52:39 +0530

    Update NN.py
    Updated to resolve problems with NNs with no hidden layers, and to make it possible to do batch gradient descent

--- NN.py
+++ NN.py
@@ -8,8 +8,9 @@ class n_layer:
         self.bias = random_matrix(self.o_l,1)
     def activate(self,m):
         self.m = m
-        self.z =add(dot(self.weights,self.m),self.bias)
+        self.z = add(dot(self.weights,self.m),self.bias)
         self.pred = sigmoid_matrix(self.z)
+
 
 class neural_net:
     def __init__(self,dim):
@@ -23,20 +24,23 @@ class neural_net:
             self.brain[i].activate(self.brain[i-1].pred)
         self.pred = self.brain[-1].pred
         self.z = self.brain[-1].z
-        
-    def backprop(self,lr,answer):
+
+    def backprop(self,answer):
         correct_answer = answer
+        weights = []
+        biases  = []
         self.cost = subtract(self.pred,correct_answer)
         delta = hadamard(self.cost,sigmoid_prime(self.z))
-
-        self.brain[-1].bias = subtract(self.brain[-1].bias,s_mult(delta,lr))
-        self.brain[-1].weights = subtract(self.brain[-1].weights,s_mult(dot(delta,self.brain[-1].m),lr)) 
+        biases.append(delta)
+        weights.append(dot(delta,transpose(self.brain[-1].m)))
         
         for i in range(len(self.brain)-2,-1,-1):
             delta = hadamard(dot(transpose(self.brain[i+1].weights),delta),sigmoid_prime(self.brain[i].z))
-            self.brain[i].bias = subtract(self.brain[i].bias,s_mult(delta,lr))
-            self.brain[i].weights = subtract(self.brain[i].weights,s_mult(dot(delta,self.brain[i].m),lr))
+            biases.append(delta)
+            weights.append(dot(delta,transpose(self.brain[i].m)))
 
+        return weights,biases
+        # This returns the gradients of all the weights and biases in case stochastic gradient descent is not used, and a sum of the these values is needed
 
 sigmoid = lambda x: 1/(1+(2.7182818284**(float(-x))))
 sig_p = lambda x: (sigmoid(x)*(1-sigmoid(x)))
